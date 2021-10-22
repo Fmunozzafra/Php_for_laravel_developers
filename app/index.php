789d9768d7bfce3d77bdf7cd54ae695e5183befb
Fmunozzafra <fmunoz@iesebre.com>
2021-10-22 17:34:54 +0200

    Codi final del segon vídeo

--- app/index.php
+++ app/index.php
@@ -1,12 +1,21 @@
 <?php
 
-require 'app/helpers.php';
+use Framework\Database\Database;
 
-require 'app/Task.php';
+require 'app/helpers.php';
 
 require 'config.php';
 
-$tasks = fetchAllTasks(connectDB($config));
+
+
+// POO -> OOP
+
+
+// WISHFUL PROGRAMMING
+$database = new Database($config); //Laravel no utilitzem gairebé mai new -> DI i Container
+$tasks = $database->selectAll('tasks');
+//$tasks = Database::selectAll('tasks'); // Crida estàtica -> sense new
+//$tasks = Task::selectAll('tasks'); -> Laravel Eloquent
 
 $greeting = greet();
 
---
+++ framework/Database/Database.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Framework\Database;
+
+use App\Models\Task;
+use PDO;
+
+class Database
+{
+    public $config;
+
+    public function __construct($config){
+        $this->config = $config;
+        $this->connection = new Connection($config);
+    }
+
+    function selectAll($table){
+        $dbh = $this->connection->connectDB($this->config);
+        $statement = $dbh->prepare("SELECT * FROM $table;");
+
+        $statement->execute();
+
+        return $statement->fetchAll(PDO::FETCH_CLASS, Task::class);
+    }
+
+    function insert(){
+
+    }
+
+}
