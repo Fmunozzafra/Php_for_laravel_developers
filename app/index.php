afad591f641d747f41ff82f1b36b4acf6cc052df
Fmunozzafra <fmunoz@iesebre.com>
2021-10-22 16:20:58 +0200

    Separar la configuració (config.php) del codi -> implementat la 0 de SOLID -> Open To Extension closed to modification

--- app/index.php
+++ app/index.php
@@ -4,15 +4,14 @@ require 'app/helpers.php';
 
 require 'app/Task.php';
 
-//$task = new Task(1, 'comprar pa', 'a la meva mare', 0);
-//var_dump($task);
-
-$user = 'debian-sys-maint';
-$pass = 'cg9aIfU5WyNOc0wp';
+require 'config.php';
 
 //Connect -> API nom connect, sense parametres entrada i tornara objecte $dbh
 try{
-    $dbh = new PDO('mysql:host=localhost;dbname=phplaraveldevs', $user,$pass);
+    $dbh = new PDO(
+        $config['database']['databasetype'] . ':host=' . $config['database']['host'] . ';dbname='$config['database']['name'],
+        $config['database']['user'],
+        $config['database']['password']);
 } catch (\Exception $e) {
     echo 'Error de connexió a la base de dades';
 }
--- config.php
+++ config.php
@@ -1,1 +1,11 @@
 <?php
+
+$config = [
+    'database' => [
+        'user' => 'debian-sys-maint',
+        'password' => 'cg9aIfU5WyNOc0wp',
+        'databasetype' => 'mysql',
+        'host' => 'localhost',
+        'name' => 'phplaraveldevs',
+    ]
+];
