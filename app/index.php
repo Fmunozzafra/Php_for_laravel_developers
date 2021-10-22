eb1c4878188c88c5573c7431692157b4d2a2e723
Fmunozzafra <fmunoz@iesebre.com>
2021-10-22 16:42:16 +0200

    Afegida funció connectDB

--- app/index.php
+++ app/index.php
@@ -6,15 +6,7 @@ require 'app/Task.php';
 
 require 'config.php';
 
-//Connect -> API nom connect, sense parametres entrada i tornara objecte $dbh
-try{
-    $dbh = new PDO(
-        $config['database']['databasetype'] . ':host=' . $config['database']['host'] . ';dbname='$config['database']['name'],
-        $config['database']['user'],
-        $config['database']['password']);
-} catch (\Exception $e) {
-    echo 'Error de connexió a la base de dades';
-}
+$dbh = connectDB($config);
 
 $statement = $dbh->prepare('SELECT * FROM tasks;');
 $statement->execute();
