9231f7a83c3927e45fa35e643964d9e0fba0729c
Fmunozzafra <fmunoz@iesebre.com>
2021-10-26 17:23:19 +0200

    Versió 0.7

--- framework/bootstrap.php
+++ framework/bootstrap.php
@@ -1,12 +1,17 @@
 <?php
 
 
+use Dotenv\Dotenv;
 use Framework\App;
 use Framework\Database\Database;
 use Framework\Database\Connection;
-//Laravel service provider
+
+$dotenv = Dotenv::createImmutable(__DIR__.'/..');
+$dotenv->load();
+
 App::bind('config', require 'config.php');
 
 App::bind('database', new Database(
         Connection::make(App::get('config')['database'])
 ));
+
